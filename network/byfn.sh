0f432c079c557e8141cf36ecb1c07bce5861b8a8
bing.xu <bing.xu@nenglian.com>
2020-04-18 16:30:27 +0800

    update chaincode

--- network/byfn.sh
+++ network/byfn.sh
@@ -9,7 +9,7 @@
 # Fabric network.
 #
 # The end-to-end verification provisions a sample Fabric network consisting of
-# two organizations, each maintaining two peers, and a Raft ordering service.
+# two organizations, each maintaining two peers, and a “solo” ordering service.
 #
 # This verification makes use of two fundamental tools, which are necessary to
 # create a functioning transactional network with digital signature validation
@@ -45,8 +45,10 @@ function printHelp() {
     echo "    -c <channel name> - channel name to use (defaults to \"mychannel\")"
     echo "    -t <timeout> - CLI timeout duration in seconds (defaults to 10)"
     echo "    -d <delay> - delay duration in seconds (defaults to 3)"
+    echo "    -f <docker-compose-file> - specify which docker-compose file use (defaults to docker-compose-cli.yaml)"
     echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
-    echo "    -l <language> - the programming language of the chaincode to deploy: go (default), javascript, or java"
+    echo "    -l <language> - the chaincode language: golang (default) or node"
+    echo "    -o <consensus-type> - the consensus-type of the ordering service: solo (default), kafka, or etcdraft"
     echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
     echo "    -a - launch certificate authorities (no certificate authorities are launched by default)"
     echo "    -n - do not deploy chaincode (abstore chaincode is deployed by default)"
@@ -59,7 +61,7 @@ function printHelp() {
     echo "	byfn.sh generate -c mychannel"
     echo "	byfn.sh up -c mychannel -s couchdb"
     echo "        byfn.sh up -c mychannel -s couchdb -i 1.4.0"
-    echo "	byfn.sh up -l javascript"
+    echo "	byfn.sh up -l node"
     echo "	byfn.sh down -c mychannel"
     echo "        byfn.sh upgrade -c mychannel"
     echo
@@ -111,7 +113,7 @@ function removeUnwantedImages() {
 }
 
 # Versions of fabric known not to work with this release of first-network
-BLACKLISTED_VERSIONS="^1\."
+BLACKLISTED_VERSIONS="^1\.0\. ^1\.1\.0-preview ^1\.1\.0-alpha"
 
 # Do some basic sanity checking to make sure that the appropriate versions of fabric
 # binaries/images are available.  In the future, additional checking for the presence
@@ -167,9 +169,10 @@ function networkUp() {
     # generate artifacts if they don't exist
     if [ ! -d "crypto-config" ]; then
         generateCerts
+        replacePrivateKey
         generateChannelArtifacts
     fi
-    COMPOSE_FILES="-f ${COMPOSE_FILE} -f ${COMPOSE_FILE_RAFT2}"
+    COMPOSE_FILES="-f ${COMPOSE_FILE}"
     if [ "${CERTIFICATE_AUTHORITIES}" == "true" ]; then
         COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_CA}"
         export BYFN_CA1_PRIVATE_KEY=$(getPrivateKeyFile org1 ca)
@@ -177,29 +180,35 @@ function networkUp() {
         export BYFN_CA2_PRIVATE_KEY=$(getPrivateKeyFile org2 ca)
         export BYFN_CA2_TLS_PRIVATE_KEY=$(getPrivateKeyFile org2 tlsca)
     fi
+    if [ "${CONSENSUS_TYPE}" == "kafka" ]; then
+        COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_KAFKA}"
+    elif [ "${CONSENSUS_TYPE}" == "etcdraft" ]; then
+        COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_RAFT2}"
+    fi
     if [ "${IF_COUCHDB}" == "couchdb" ]; then
         COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_COUCH}"
     fi
-    IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILES} up -d 2>&1
+    docker-compose ${COMPOSE_FILES} up -d 2>&1
     docker ps -a
     if [ $? -ne 0 ]; then
         echo "ERROR !!!! Unable to start network"
         exit 1
     fi
 
-    echo "Sleeping 15s to allow Raft cluster to complete booting"
-    sleep 15
+    if [ "$CONSENSUS_TYPE" == "kafka" ]; then
+        sleep 1
+        echo "Sleeping 10s to allow $CONSENSUS_TYPE cluster to complete booting"
+        sleep 9
+    fi
 
-    if [ "${NO_CHAINCODE}" != "true" ]; then
-        echo Vendoring Go dependencies ...
-        pushd ../chaincode/chaincode_token/go
-        GO111MODULE=on go mod vendor
-        popd
-        echo Finished vendoring Go dependencies
+    if [ "$CONSENSUS_TYPE" == "etcdraft" ]; then
+        sleep 1
+        echo "Sleeping 15s to allow $CONSENSUS_TYPE cluster to complete booting"
+        sleep 14
     fi
 
     # now run the end to end script
-    docker exec cli.demo scripts/script.sh $CHANNEL_NAME $CLI_DELAY $CC_SRC_LANGUAGE $CLI_TIMEOUT $VERBOSE $NO_CHAINCODE
+    docker exec cli.demo scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE $NO_CHAINCODE
     if [ $? -ne 0 ]; then
         echo "ERROR !!!! Test failed"
         exit 1
@@ -223,11 +232,18 @@ function upgradeNetwork() {
         mkdir -p $LEDGERS_BACKUP
 
         export IMAGE_TAG=$IMAGETAG
-        COMPOSE_FILES="-f ${COMPOSE_FILE} -f ${COMPOSE_FILE_RAFT2}"
+        COMPOSE_FILES="-f ${COMPOSE_FILE}"
         if [ "${CERTIFICATE_AUTHORITIES}" == "true" ]; then
             COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_CA}"
-            export BYFN_CA1_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org1.example.com/ca && ls *_sk)
-            export BYFN_CA2_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org2.example.com/ca && ls *_sk)
+            export BYFN_CA1_PRIVATE_KEY=$(getPrivateKeyFile org1 ca)
+            export BYFN_CA1_TLS_PRIVATE_KEY=$(getPrivateKeyFile org1 tlsca)
+            export BYFN_CA2_PRIVATE_KEY=$(getPrivateKeyFile org2 ca)
+            export BYFN_CA2_TLS_PRIVATE_KEY=$(getPrivateKeyFile org2 tlsca)
+        fi
+        if [ "${CONSENSUS_TYPE}" == "kafka" ]; then
+            COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_KAFKA}"
+        elif [ "${CONSENSUS_TYPE}" == "etcdraft" ]; then
+            COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_RAFT2}"
         fi
         if [ "${IF_COUCHDB}" == "couchdb" ]; then
             COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_COUCH}"
@@ -263,7 +279,7 @@ function upgradeNetwork() {
             docker-compose $COMPOSE_FILES up -d --no-deps $PEER
         done
 
-        docker exec cli.demo scripts/upgrade_to_v14.sh $CHANNEL_NAME $CLI_DELAY $CC_SRC_LANGUAGE $CLI_TIMEOUT $VERBOSE
+        docker exec cli.demo sh -c "SYS_CHANNEL=$CH_NAME && scripts/upgrade_to_v14.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE"
         if [ $? -ne 0 ]; then
             echo "ERROR !!!! Test failed"
             exit 1
@@ -276,7 +292,9 @@ function upgradeNetwork() {
 # Tear down running network
 function networkDown() {
     # stop containers also in addition to org1 and org2
-    docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH -f $COMPOSE_FILE_RAFT2 -f $COMPOSE_FILE_CA down --volumes --remove-orphans
+    # stop kafka and zookeeper containers in case we're running with kafka consensus-type
+    #  docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH -f $COMPOSE_FILE_KAFKA -f $COMPOSE_FILE_RAFT2 -f $COMPOSE_FILE_CA -f $COMPOSE_FILE_ORG3 down --volumes --remove-orphans
+    docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_CA down --volumes --remove-orphans
 
     # Don't remove the generated artifacts -- note, the ledgers are always removed
     if [ "$MODE" != "restart" ]; then
@@ -289,8 +307,42 @@ function networkDown() {
         removeUnwantedImages
         # remove orderer block and other channel configuration transactions and certs
         rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
+        # remove the docker-compose yaml file that was customized to the example
         rm -f docker-compose-e2e.yaml
         rm -f connection-*
+    fi
+}
+
+# Using docker-compose-e2e-template.yaml, replace constants with private key file names
+# generated by the cryptogen tool and output a docker-compose.yaml specific to this
+# configuration
+function replacePrivateKey() {
+    # sed on MacOSX does not support -i flag with a null extension. We will use
+    # 't' for our back-up's extension and delete it at the end of the function
+    ARCH=$(uname -s | grep Darwin)
+    if [ "$ARCH" == "Darwin" ]; then
+        OPTS="-it"
+    else
+        OPTS="-i"
+    fi
+
+    # Copy the template to the file that will be modified to add the private key
+    cp docker-compose-e2e-template.yaml docker-compose-e2e.yaml
+
+    # The next steps will replace the template's contents with the
+    # actual values of the private key file names for the two CAs.
+    CURRENT_DIR=$PWD
+    cd crypto-config/peerOrganizations/org1.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd "$CURRENT_DIR"
+    sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+    cd crypto-config/peerOrganizations/org2.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd "$CURRENT_DIR"
+    sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+    # If MacOSX, remove the temporary backup of the docker-compose file
+    if [ "$ARCH" == "Darwin" ]; then
+        rm docker-compose-e2e.yamlt
     fi
 }
 
@@ -389,8 +441,19 @@ function generateChannelArtifacts() {
     echo "##########################################################"
     # Note: For some unknown reason (at least for now) the block file can't be
     # named orderer.genesis.block or the orderer will fail to launch!
+    echo "CONSENSUS_TYPE="$CONSENSUS_TYPE
     set -x
-    configtxgen -profile TwoOrgsOrdererGenesis -channelID $SYS_CHANNEL -outputBlock ./channel-artifacts/genesis.block
+    if [ "$CONSENSUS_TYPE" == "solo" ]; then
+        configtxgen -profile TwoOrgsOrdererGenesis -channelID $SYS_CHANNEL -outputBlock ./channel-artifacts/genesis.block
+    elif [ "$CONSENSUS_TYPE" == "kafka" ]; then
+        configtxgen -profile SampleDevModeKafka -channelID $SYS_CHANNEL -outputBlock ./channel-artifacts/genesis.block
+    elif [ "$CONSENSUS_TYPE" == "etcdraft" ]; then
+        configtxgen -profile SampleMultiNodeEtcdRaft -channelID $SYS_CHANNEL -outputBlock ./channel-artifacts/genesis.block
+    else
+        set +x
+        echo "unrecognized CONSESUS_TYPE='$CONSENSUS_TYPE'. exiting"
+        exit 1
+    fi
     res=$?
     set +x
     if [ $res -ne 0 ]; then
@@ -402,7 +465,8 @@ function generateChannelArtifacts() {
     echo "### Generating channel configuration transaction 'channel.tx' ###"
     echo "#################################################################"
     set -x
-    configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/$CHANNEL_NAME.tx -channelID $CHANNEL_NAME
+    configtxgen -profile TwoOrgsChannel -outputCreateChannelTx \
+        ./channel-artifacts/$CHANNEL_NAME.tx -channelID $CHANNEL_NAME
     res=$?
     set +x
     if [ $res -ne 0 ]; then
@@ -439,6 +503,9 @@ function generateChannelArtifacts() {
     echo
 }
 
+# Obtain the OS and Architecture string that will be used to select the correct
+# native binaries for your platform, e.g., darwin-amd64 or linux-amd64
+OS_ARCH=$(echo "$(uname -s | tr '[:upper:]' '[:lower:]' | sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
 # timeout duration - the duration the CLI should wait for a response from
 # another container before giving up
 CLI_TIMEOUT=10
@@ -452,16 +519,19 @@ CHANNEL_NAME="mychannel"
 COMPOSE_FILE=docker-compose-cli.yaml
 #
 COMPOSE_FILE_COUCH=docker-compose-couch.yaml
+# kafka and zookeeper compose file
+COMPOSE_FILE_KAFKA=docker-compose-kafka.yaml
 # two additional etcd/raft orderers
 COMPOSE_FILE_RAFT2=docker-compose-etcdraft2.yaml
 # certificate authorities compose file
 COMPOSE_FILE_CA=docker-compose-ca.yaml
 #
 # use golang as the default language for chaincode
-CC_SRC_LANGUAGE=go
+LANGUAGE=golang
 # default image tag
-CFG_IMAGE_TAG=$(cat .env | grep 'IMAGE_TAG')
-IMAGETAG=${CFG_IMAGE_TAG#*=}
+IMAGETAG=$(cat .env | grep 'IMAGE_TAG')
+# default consensus type
+CONSENSUS_TYPE="solo"
 # Parse commandline args
 if [ "$1" = "-m" ]; then # supports old usage, muscle memory is powerful!
     shift
@@ -484,7 +554,7 @@ else
     exit 1
 fi
 
-while getopts "h?c:t:d:s:l:i:anv" opt; do
+while getopts "h?c:t:d:f:s:l:i:o:anv" opt; do
     case "$opt" in
     h | \?)
         printHelp
@@ -499,14 +569,20 @@ while getopts "h?c:t:d:s:l:i:anv" opt; do
     d)
         CLI_DELAY=$OPTARG
         ;;
+    f)
+        COMPOSE_FILE=$OPTARG
+        ;;
     s)
         IF_COUCHDB=$OPTARG
         ;;
     l)
-        CC_SRC_LANGUAGE=$OPTARG
+        LANGUAGE=$OPTARG
         ;;
     i)
         IMAGETAG=$(go env GOARCH)"-"$OPTARG
+        ;;
+    o)
+        CONSENSUS_TYPE=$OPTARG
         ;;
     a)
         CERTIFICATE_AUTHORITIES=true
@@ -538,6 +614,7 @@ elif [ "${MODE}" == "down" ]; then ## Clear the network
     networkDown
 elif [ "${MODE}" == "generate" ]; then ## Generate Artifacts
     generateCerts
+    replacePrivateKey
     generateChannelArtifacts
 elif [ "${MODE}" == "restart" ]; then ## Restart the network
     networkDown
--- network/scripts/script.sh
+++ network/scripts/script.sh
@@ -11,34 +11,27 @@ echo "Build your first network (BYFN) end-to-end test"
 echo
 CHANNEL_NAME="$1"
 DELAY="$2"
-CC_SRC_LANGUAGE="$3"
+LANGUAGE="$3"
 TIMEOUT="$4"
 VERBOSE="$5"
 NO_CHAINCODE="$6"
 : ${CHANNEL_NAME:="mychannel"}
 : ${DELAY:="3"}
-: ${CC_SRC_LANGUAGE:="go"}
+: ${LANGUAGE:="golang"}
 : ${TIMEOUT:="10"}
 : ${VERBOSE:="false"}
 : ${NO_CHAINCODE:="false"}
-CC_SRC_LANGUAGE=$(echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:])
+LANGUAGE=$(echo "$LANGUAGE" | tr [:upper:] [:lower:])
 COUNTER=1
-MAX_RETRY=20
-PACKAGE_ID=""
+MAX_RETRY=10
 
-if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ]; then
-    CC_RUNTIME_LANGUAGE=golang
-    CC_SRC_PATH="github.com/chaincode/chaincode_token/go/"
-elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
-    CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
-    CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/abstore/javascript/"
-elif [ "$CC_SRC_LANGUAGE" = "java" ]; then
-    CC_RUNTIME_LANGUAGE=java
-    CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/abstore/java/"
-else
-    echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
-    echo Supported chaincode languages are: go, javascript, java
-    exit 1
+CC_SRC_PATH="github.com/chaincode/chaincode_token/go/"
+if [ "$LANGUAGE" = "node" ]; then
+  CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+fi
+
+if [ "$LANGUAGE" = "java" ]; then
+  CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/java/"
 fi
 
 echo "Channel name : "$CHANNEL_NAME
@@ -47,34 +40,34 @@ echo "Channel name : "$CHANNEL_NAME
 . scripts/utils.sh
 
 createChannel() {
-    setGlobals 0 1
+  setGlobals 0 1
 
-    if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
-        set -x
-        peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx >&log.txt
-        res=$?
-        set +x
-    else
-        set -x
-        peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
-        res=$?
-        set +x
-    fi
-    cat log.txt
-    verifyResult $res "Channel creation failed"
-    echo "===================== Channel '$CHANNEL_NAME' created ===================== "
-    echo
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Channel creation failed"
+  echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+  echo
 }
 
 joinChannel() {
-    for org in 1 2; do
-        for peer in 0 1; do
-            joinChannelWithRetry $peer $org
-            echo "===================== peer${peer}.org${org} joined channel '$CHANNEL_NAME' ===================== "
-            sleep $DELAY
-            echo
-        done
+  for org in 1 2; do
+    for peer in 0 1; do
+      joinChannelWithRetry $peer $org
+      echo "===================== peer${peer}.org${org} joined channel '$CHANNEL_NAME' ===================== "
+      sleep $DELAY
+      echo
     done
+  done
 }
 
 ## Create channel
@@ -92,54 +85,29 @@ echo "Updating anchor peers for org2..."
 updateAnchorPeers 0 2
 
 if [ "${NO_CHAINCODE}" != "true" ]; then
-    ## at first we package the chaincode
-    packageChaincode 1 0 1
-
-    ## Install chaincode on peers
-    for org in 1 2; do
-        for peer in 0 1; do
-            echo "Installing chaincode on peer${peer}.org${org}..."
-            installChaincode ${peer} ${org}
-        done
+  ## Install chaincode on peers
+  for org in 1 2; do
+    for peer in 0 1; do
+      echo "Installing chaincode on peer${peer}.org${org}..."
+      installChaincode ${peer} ${org}
     done
+  done
 
-    ## query whether the chaincode is installed
-    queryInstalled 0 1
+  # Instantiate chaincode on peer0.org2
+  echo "Instantiating chaincode on peer0.org2..."
+  instantiateChaincode 0 2
 
-    ## approve the definition for org1 and org2
-    approveForMyOrg 1 0 1
-    approveForMyOrg 1 0 2
+  # Query chaincode on peer0.org1
+  echo "Querying chaincode on peer1.org1..."
+  chaincodeQuery 0 1 100
 
-    ## now that we know for sure both orgs have approved, commit the definition
-    commitChaincodeDefinition 1 0 1 0 2
+  # Invoke chaincode on peer0.org1 and peer0.org2
+  echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+  chaincodeInvoke 0 1 0 2
 
-    ## query on both orgs to see that the definition committed successfully
-    queryCommitted 1 0 1
-    queryCommitted 1 0 2
-
-    ## invoke init
-    chaincodeInvoke 1 0 1 0 2
-
-    ## Query chaincode on peer0.org1
-    #echo "Querying chaincode on peer0.org1..."
-    #chaincodeQuery 0 1 100
-
-    ## Invoke chaincode on peer0.org1 and peer0.org2
-    #echo "Sending invoke transaction on peer0.org1 peer0.org2..."
-    #chaincodeInvoke 0 0 1 0 2
-
-    ## Query chaincode on peer0.org1
-    #echo "Querying chaincode on peer0.org1..."
-    #chaincodeQuery 0 1 90
-
-    ## Install chaincode on peer1.org2
-    #echo "Installing chaincode on peer1.org2..."
-    #installChaincode 1 2
-
-    ## Query on chaincode on peer1.org2, check if the result is 90
-    #echo "Querying chaincode on peer1.org2..."
-    #chaincodeQuery 1 2 90
-
+  # Query on chaincode on peer1.org2, check if the result is 90
+  echo "Querying chaincode on peer1.org2..."
+  chaincodeQuery 1 2 90
 fi
 
 echo
